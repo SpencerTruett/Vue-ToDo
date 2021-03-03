a9ac25d8f217770fa3803899c60483617c6bcd11
Spencer Truett <srtruett1993@gmail.com>
2021-03-03 10:33:57 -0600

    components initially connected

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>Here is the To Do List</h3>
+  </div>
+</template>
+
+<script>
+import { todoItems } from "../data";
+
+export default {
+  data() {
+    return {
+      todos: [...todoItems]
+    };
+  }
+};
+</script>
+
+<style></style>
