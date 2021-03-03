d6995dcabb982f340123174e4c65f7aefb10eb12
Spencer Truett <srtruett1993@gmail.com>
2021-03-03 11:09:44 -0600

    added computed values

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3>ToDo List</h3>
+    <h4>{{ itemsRemaining }}</h4>
     <div v-for="item in todos" :key="item.id">
       <todo-item :todo="item" @status-change="handleStatusChange" />
     </div>
@@ -22,6 +23,18 @@ export default {
     handleStatusChange(item) {
       item.complete = !item.complete;
       console.log(item);
+    }
+  },
+  computed: {
+    itemsRemaining() {
+      const remaining = this.todos.filter(t => !t.complete).length;
+      if (remaining === 1) {
+        return "There is 1 item left to do today";
+      }
+      if (remaining === 0) {
+        return "There are no item left to do today!";
+      }
+      return `There are ${remaining} items left to do today`;
     }
   }
 };
